e42990b3e20f9f05719cada1b8832ad018a30dfa
JointFury <liamcrouch2009@gmail.com>
2025-11-15 17:50:09 +0000

    idk

---
+++ play.pokemonshowdown.com/config/custom-formats.ts
@@ -0,0 +1,12 @@
+export const Formats: FormatList = [
+	{
+		section: "Joint",
+	},
+{
+	name: "Joint",
+	mod: 'joint',
+	gen: 9,
+	ruleset: ['Standard NatDex', '-allpokemon', '+pikachu'],
+},
+
+];
